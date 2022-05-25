b6b02b5c5cf6540fc8c1173b2bc0d3ff6be35ac8
Ermal ALIRAJ <ermal.aliraj@gmail.com>
2022-05-25 15:36:34 +0200

    Student controller added comments

--- campus-ms/campus-student-ms/src/main/java/com/ea/campus/ms/student/controller/StudentController.java
+++ campus-ms/campus-student-ms/src/main/java/com/ea/campus/ms/student/controller/StudentController.java
@@ -22,16 +22,34 @@ public class StudentController {
         this.studentDtoMapper = studentDtoMapper;
     }
 
+    /**
+     * Get list of all students
+     *
+     * REST call:
+     * GET http://localhost:8085/students
+     */
     @GetMapping
     public PageDto<StudentDto> getAll(StudentQueryParam queryParam, Pageable page) {
         return studentDtoMapper.toPageDto(studentService.getAll(queryParam, page));
     }
 
+    /**
+     * Get students count
+     *
+     * REST call:
+     * GET http://localhost:8085/students
+     */
     @GetMapping("/count")
     public long count(StudentQueryParam queryParam) {
         return studentService.count(queryParam);
     }
 
+    /**
+     * Get student by ID
+     *
+     * REST call:
+     * GET http://localhost:8085/students/123
+     */
     @GetMapping("/{id}")
     public StudentDto findById(@PathVariable String id) {
         Student student = studentService.getById(id)
@@ -39,19 +57,51 @@ public class StudentController {
         return studentDtoMapper.toDto(student);
     }
 
-    @DeleteMapping("/{id}")
-    public void deleteById(@PathVariable String id) {
-        studentService.deleteById(id);
-    }
-
+    /**
+     * Create a new student
+     *
+     * REST call:
+     * POST http://localhost:8085/students/123
+     * Body: { name:"Ermal" , "surname" : "Aliraj"}
+     */
     @PostMapping
     public StudentDto create(@RequestBody StudentDto studentDto) {
         Student student = studentDtoMapper.toEntity(studentDto);
         return studentDtoMapper.toDto(studentService.save(student));
     }
 
+    /**
+     * Update student with id passed in parameter
+     *
+     * REST call:
+     * PUT http://localhost:8085/students/123
+     * Body: { name:"Ermal2" , "surname" : "Aliraj222"}
+     */
+    @PutMapping("/{id}")
+    public void update(@PathVariable String id, @RequestBody StudentDto studentDto) {
+        Student student = studentDtoMapper.toEntity(studentDto);
+        studentService.save(student);
+    }
+
+    /**
+     * Delete all students
+     *
+     * REST call:
+     * DELETE http://localhost:8085/students
+     */
     @DeleteMapping
     public void deleteAll() {
         studentService.deleteAll();
     }
+
+    /**
+     * Delete the student with ID passed in parameter
+     * REST call:
+     * DELETE http://localhost:8085/students/123
+     */
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable String id) {
+        studentService.deleteById(id);
+    }
+
 }
